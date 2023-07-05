efc3ef9d80c417446b7971f62b105223ecb5e27b
Fausto <ofaustojoaquindejesus@gmail.com>
2023-07-05 15:03:47 -0300

    configuracion arranque de servidor

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 // Imports
 const cors = require('cors');
 const express = require('express');
-
+const helmet = require('helmet');
+const morgan = require('morgan');
 const path = require('path');
+require('dotenv').config();
+require('ejs');
 
 const app = express();
+const port = process.env.PORT || 5000
+
 
 // Middlewares
 // TODO: Implementar middlewares
+
+
+app.use(cors());
+//app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -16,5 +30,10 @@ app.use('/api', require('./routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
+app.use((req, res, next) => {
+    return res.status(404).render('404');
+
+})
+
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(port, () => console.log(`Server on port http://localhost:${port}`));
