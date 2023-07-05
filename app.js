a6cfc63720691ccfb836aa74bdf586ef1219efc0
Fausto <ofaustojoaquindejesus@gmail.com>
2023-07-05 16:15:48 -0300

    correccion de errores

--- app.js
+++ app.js
@@ -30,12 +30,12 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/api', require('./routes/reserva.routes'));
+app.use('/', require('./routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
 app.use((req, res, next) => {
-    return res.status(404).render('404');
+    return res.status(404);
 
 })
 
